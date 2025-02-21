c2942536e7f2134caf3e4e6288453ee587447a41
Sanidhya <sanidhya.22b0121062@abes.ac.in>
2025-02-21 11:59:18 +0530

    Adding home and refreshing it to get back to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Route,Routes} from "react-router-dom"
+import {Link, Route,Routes} from "react-router-dom"
 import Home from './Components/Home'
 import Details from './Components/Details'
 const App = () => {
   return (
     <div className='h-screen w-screen flex '>
+      <Link to="/"className='absolute left-[17%] top-[3%]'>Home</Link>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/Details/:id" element={<Details/>} />
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,24 +1,42 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Nav from "./Nav"
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { ProductContext } from '../utils/Context'
 import Loading from './Loading'
+import axios from '../utils/axios'
 const Home = () => {
   const [products]=useContext(ProductContext);
-  console.log(products)
+  const {search}=useLocation();
+  const catagory=decodeURIComponent(search.split("=")[1]);
+  const [filteredproducts,setfilteredproducts]=useState(null);
+  const getproductcategory=async()=>{
+
+     try {
+       const {data}=await axios.get(`/products/category/${catagory}`)
+       setfilteredproducts(data)
+    }
+    catch(error){
+        console.log(error)
+  }
+}
+  useEffect(()=>{
+    if(!filteredproducts||catagory=="undefined") setfilteredproducts(products);
+    if(catagory!="undefined") getproductcategory();
+  }
+  ,[catagory,products]);
   return products ? ( 
   <>
     <Nav/>
     
     <div className='h-full w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto'>
-    {products.map((p,i)=>( <Link  key={p.id} to="/Details/1" className='card mr-3 mb-3 p-3 rounded shadow-lg w-[18%] h-[30vh] flex-col flex justify-center items-center'>
+    {filteredproducts&&filteredproducts.map((p,i)=>( <Link  key={p.id} to="/Details/1" className='card mr-3 mb-3 p-3 rounded shadow-lg w-[18%] h-[30vh] flex-col flex justify-center items-center'>
       <div className='w-full hover:scale-110 h-[80%] bg-contain bg-no-repeat bg-center mb-3' 
       style={{
         backgroundImage:`url(${p.image})`,
       }}></div>
       <h1 className='hover:text-blue-300'>{p.title}</h1>
     </Link>))}
-   
+  
 
 
 
